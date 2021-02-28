d9a876428fd2e3dce74222ab0e6ec0b7d9388ee4
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-02-28 12:48:59 +0900

    Move descriptions to String resource

--- app/src/main/java/com/example/androiddevchallenge/Dog.kt
+++ app/src/main/java/com/example/androiddevchallenge/Dog.kt
@@ -1,11 +1,12 @@
 package com.example.androiddevchallenge
 
 import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 
 data class Dog(
     val id: String,
     val breed: String,
     val origin: String,
     @DrawableRes val drawableResId: Int,
-    val description: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    @StringRes val descriptionResId: Int = R.string.lorem_ipsum
 )
--- app/src/main/java/com/example/androiddevchallenge/DogDetail.kt
+++ app/src/main/java/com/example/androiddevchallenge/DogDetail.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -30,6 +31,6 @@ fun DogDetail(dog: Dog) {
         Spacer(Modifier.height(16.dp))
         Text("Origin: ${dog.origin}", style = MaterialTheme.typography.h6)
         Spacer(Modifier.height(16.dp))
-        Text(dog.description, style = MaterialTheme.typography.body2)
+        Text(stringResource(dog.descriptionResId), style = MaterialTheme.typography.body2)
     }
 }
