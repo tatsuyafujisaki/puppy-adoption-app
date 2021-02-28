77fb575af313b2a6b91efc47effd6f270bd39c58
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2021-02-28 11:34:44 +0900

    Add a list of dogs

---
+++ app/src/main/java/com/example/androiddevchallenge/Dog.kt
@@ -0,0 +1,10 @@
+package com.example.androiddevchallenge
+
+import androidx.annotation.DrawableRes
+
+data class Dog(
+    val breed: String,
+    val origin: String,
+    @DrawableRes val drawableResId: Int,
+    val description: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+)
--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -1,61 +1,120 @@
-/*
- * Copyright 2021 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.androiddevchallenge
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.androiddevchallenge.ui.theme.MyTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyTheme {
-                MyApp()
+            MyApp {
+                DogList(dogs) {
+                }
             }
         }
     }
 }
 
-// Start building your app here!
 @Composable
-fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+fun MyApp(content: @Composable () -> Unit) {
+    MaterialTheme {
+        Surface {
+            content()
+        }
     }
 }
 
-@Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
-fun LightPreview() {
-    MyTheme {
-        MyApp()
+fun DogList(
+    dogs: List<Dog>,
+    onClick: () -> Unit,
+) {
+    LazyColumn(Modifier.fillMaxWidth()) {
+        items(dogs) {
+            DogListItem(
+                dog = it,
+                onClick = onClick
+            )
+            Divider(color = Color.LightGray)
+        }
     }
 }
 
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
-fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        MyApp()
+fun DogListItem(
+    dog: Dog,
+    onClick: () -> Unit
+) {
+    Row(
+        Modifier
+            .clickable(onClick = onClick)
+            .padding(8.dp)
+            .background(MaterialTheme.colors.surface)
+            .padding(8.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(dog.drawableResId),
+            contentDescription = null,
+            modifier = Modifier
+                .size(64.dp)
+                .clip(CircleShape)
+                .border(2.dp, Color.LightGray, CircleShape),
+            contentScale = ContentScale.Crop,
+        )
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text(dog.breed, style = MaterialTheme.typography.h6)
+            Text(dog.origin, style = MaterialTheme.typography.subtitle1, color = Color.LightGray)
+        }
     }
 }
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    MyApp {
+        DogList(dogs) {}
+    }
+}
+
+val dogs = listOf(
+    Dog("Shiba", "Japan", R.drawable.shiba),
+    Dog("American Cocker Spaniel", "United States", R.drawable.american_cocker_spaniel),
+    Dog("Pug", "China", R.drawable.pug),
+    Dog("West Highland White Terrier", "Scotland", R.drawable.west_highland_white_terrier),
+    Dog("Chihuahua", "Mexico", R.drawable.chihuahua),
+    Dog("Dalmatian", "Croatia", R.drawable.dalmatian),
+    Dog("Dachshund", "Germany", R.drawable.dachshund),
+    Dog("Dobermann", "Germany", R.drawable.dobermann),
+    Dog("Labrador Retriever", "United Kingdom", R.drawable.labrador_retriever),
+    Dog("Beagle", "England", R.drawable.beagle)
+)
